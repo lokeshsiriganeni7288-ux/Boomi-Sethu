ebb3537efcb15933995a3bea165432f1001223b7
Lokesh Siriganeni <lokeshsiriganeni123@gmail.com>
2026-07-19 19:32:46 +0530

    pushing folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import HomePage from "./pages/HomePage";
+import HomePage from "./Pages/HomePage";
 import { HelmetProvider } from "react-helmet-async";
 function App() {
   return (
